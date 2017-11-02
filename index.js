16772faf2cf3f9ec1e69af42a856bef72be7dbdc
machimura <machimura@tagbangers.co.jp>
2017-11-02 12:15:46 +0900

    最小のレスポンス

--- index.js
+++ index.js
@@ -12,27 +12,25 @@ var server = app.listen(process.env.PORT || 3000, function(){
 app.post('/hook', function(req, res, next){
     res.status(200).end();
     console.log(req);
-    for (var event of req.body.events){
-        if (event.type == 'message' && event.message.text == 'ハロー'){
-            var headers = {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + LINE_CHANNEL_ACCESS_TOKEN
-            }
-            var body = {
-                replyToken: event.replyToken,
-                messages: [{
-                    type: 'text',
-                    text: 'こんにちは'
-                }]
-            }
-            var url = 'https://api.line.me/v2/bot/message/reply';
-            request({
-                url: url,
-                method: 'POST',
-                headers: headers,
-                body: body,
-                json: true
-            });
-        }
+    const options = {
+      method: 'POST',
+      uri: 'https://api.line.me/v2/bot/message/reply',
+      body: {
+        replyToken: req.body.events[0].replyToken,
+        messages: [{
+          type: 'text',
+          text: req.body.events[0].message.text // ここに指定した文字列がボットの発言になる
+        }]
+      },
+      auth: {
+        bearer: LINE_CHANNEL_ACCESS_TOKEN; // ここは自分のtokenに書き換える
+      },
+      json: true
     }
+    request(options, (err, response, body) => {
+      console.log(JSON.stringify(response))
+    });
+    res.send('OK')
+  });
+  }
 });
