3af43b4f404a2a6f3e0e69c649e781c231a002e0
machimura <machimura@tagbangers.co.jp>
2017-11-02 12:21:40 +0900

    token

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const LINE_CHANNEL_ACCESS_TOKEN = 'A1j5wzdp9/zZFySROIOu+xRKYiwprNdoXV5EQNyvw1XJvnc1GFd19TTfZebfukKzmZ46IcD3gbUL8YcmjSoiRDOjmzXnDwAn1v+7IS18G2YYcwjOaHbTGGQfDaVNc4Fy+3OPzvcfyjynqmw4d4E6WAdB04t89/1O/w1cDnyilFU=';
-
 var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
@@ -23,7 +21,7 @@ app.post('/hook', function(req, res, next){
         }]
       },
       auth: {
-        bearer: LINE_CHANNEL_ACCESS_TOKEN; // ここは自分のtokenに書き換える
+        bearer: 'A1j5wzdp9/zZFySROIOu+xRKYiwprNdoXV5EQNyvw1XJvnc1GFd19TTfZebfukKzmZ46IcD3gbUL8YcmjSoiRDOjmzXnDwAn1v+7IS18G2YYcwjOaHbTGGQfDaVNc4Fy+3OPzvcfyjynqmw4d4E6WAdB04t89/1O/w1cDnyilFU='; // ここは自分のtokenに書き換える
       },
       json: true
     }
