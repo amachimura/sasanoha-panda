cf3e176e2d9d8e52f1933d04f0046cc2f226045f
diodario <machimura.akira@gmail.com>
2018-01-03 18:36:19 +0900

    googleの認証ファイルを追加

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+app.use(express.static('public'));
 
 var server = app.listen(process.env.PORT || 3000, function(){
     console.log("listening port:" + server.address().port);
